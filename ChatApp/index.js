55edf26610e0ee96d1315383cb022c66b20d308c
Moitrayee <moitrayeedey575@gmail.com>
2024-07-18 14:40:59 +0530

    added web socket implementation

--- ChatApp/index.js
+++ ChatApp/index.js
@@ -1,9 +1,17 @@
 const express = require('express');
 const http = require('http');
 const path = require('path');
+const { Server } = require('socket.io');
 
 const app = express();
 const server = http.createServer(app);
+//  this will handle our sockets
+const io = new Server(server);
+io.on('connection', (client) => {
+    client.on('user-message', (message) => {
+        io.emit('message', message);
+    });
+})
 
 app.use(express.static(path.resolve('./public')));
 app.get('/', (req, res) => {
